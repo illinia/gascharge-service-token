f003c27993f4a91fe0d2b3afd7063e768ec9a82f
김태민 <seolin1218@gmail.com>
2023-03-12 12:53:23 +0900

    init

---
+++ src/main/java/com/gascharge/taemin/service/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.gascharge.taemin.service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.gascharge.taemin.service")
+public class AutoConfig {
+}
